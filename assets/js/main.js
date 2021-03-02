1987d7657cda705a25011ebb35a71488f066f312
France900 <francesco.em.livio@gmail.com>
2021-03-02 17:56:38 +0100

    esercizio finito
    adesso provo un pò di bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,32 @@
 
 
 // chiedo mail
-var email = prompt("quale è la tua email??")
+var pomptImpput = prompt("quale è la tua email??")
 
 //creo array
-var emails = ["marco", "giorgio", "luca", "giuseppe", "jacopo", "filippo"];
+var archivio = ["", "marco@gmail.com", "giorgio@gmail.com", "claudio@gmail.com", "andrea.fr.gori@gmail.com", "francesco.em.livio@gmail.com", "francem.livio@gmail.com"];
 
 //controllo se è nel database
 var soldatino = false;
 //scorro array
-for (var i = 0; i < emails.length; i++) {
-   if (emails[i] == email) {
+for (var i = 0; i < archivio.length; i++) {
+   if (archivio[i] == pomptImpput) {
     soldatino = true
   }
 }
 
 if (soldatino) {
-  console.log("la tua email è stata registrata!");
+  console.log("Hai eseguito l'accesso!");
+  document.getElementById('result').innerHTML = "Hai eseguito l'accesso!"; // trasporta scritte in js in html
 } else {
-  console.log("mi spiace non fai parte di questa mailing list");
+  console.log("Spiacenti ma questa mail non è registrata.");
+  document.getElementById('result').innerHTML = "Spiacenti ma questa mail non è registrata."; // trasporta scritte in js in html
 }
+
+
+//bonus
+alert("adesso il sistema lancerà un dado per te e uno per se stesso. vediamo chi vince!")
+var numberCPU = Math.floor(Math.random()*(6-1+1)+1);
+console.log(numberCPU);
+var numberPlayer = Math.floor(Math.random()*(6-1+1)+1);
+console.log(numberPlayer);
